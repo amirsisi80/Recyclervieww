af510ff82a7b8669b993ebd14ef046f13308783a
amirsisi80 <amirghosthossein@gmail.com>
2024-05-09 02:40:50 +0330

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerview_items = findViewById(R.id.recyclerview_items);
+        AdapterName adapterName = new AdapterName();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerview_items.setLayoutManager(layoutManager);
+        recyclerview_items.setAdapter(adapterName);
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/TempListItem.java
@@ -0,0 +1,55 @@
+package com.example.recyclerview;
+
+public class TempListItem {
+    private String[] name = {
+
+            "Emily",
+            "James",
+            "Sophia",
+            "William",
+            "Olivia",
+            "Benjamin",
+            "Ava",
+            "Alexander",
+            "Mia",
+            "Michael",
+            "Charlotte",
+            "Daniel",
+            "Amelia",
+            "Matthew",
+            "Harper",
+            "Christopher",
+            "Ella",
+            "David"
+
+    };
+    private int[] avatar = {
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1,
+            R.drawable.avatar1
+    };
+
+    public String[] getName() {
+        return name;
+    }
+
+    public int[] getAvatar() {
+        return avatar;
+    }
+
+}
